d206e8012247ba231a09fba945bbda87e994fcc4
Juangabrielgonzalez222 <63814245+Juangabrielgonzalez222@users.noreply.github.com>
2022-04-22 18:35:04 -0300

    Delete Main.py

--- Main.py
+++
@@ -1,8 +0,0 @@
-from ManejadorViajero import ManejadorViajero
-from Menu import Menu
-
-if __name__== '__main__':
-    manejadorViajero=ManejadorViajero()
-    manejadorViajero.cargarDesdeArchivo()
-    menu=Menu()
-    menu.lanzarMenu(manejadorViajero)
